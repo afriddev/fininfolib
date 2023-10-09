26817c2e2e7a5a784c3b216c0f83024ed8642498
AFRID SHAIK <afridayan01@gmail.com>
2023-10-09 09:32:21 +0000

    Add files via upload

---
+++ app/contact/page.tsx
@@ -0,0 +1,23 @@
+import BottomFooter from "@/components/bottomFooter"
+import LetsTalk from "@/components/letsTalk"
+
+const Contact: () => void = () => {
+    return (
+        <>
+            <div>
+                <div className="flex w-full h-fit bg-gradient-to-t from-o1 to-o2 ">
+                    <label className="py-4 text-3xl font-bold text-white pl-14 ">
+                        Contact us </label>
+                </div>
+
+            </div>
+            <div className="mt-10">
+                <LetsTalk />
+            </div>
+            <div className="mt-20">
+                <BottomFooter />
+            </div>
+        </>
+    )
+}
+export default Contact
