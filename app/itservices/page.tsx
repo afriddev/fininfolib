36141ba67aa8be84a916288274701c8a9aace41c
AFRID SHAIK <afridayan01@gmail.com>
2023-10-09 09:55:22 +0000

    Add files via upload

---
+++ app/itservices/page.tsx
@@ -0,0 +1,61 @@
+const IteServices: () => void = () => {
+    return (
+        <>
+            <div>
+                <div className=" p-6 bg-gradient-to-b from-o1 to-o2 w-full h-fit flex flex-col">
+                    <h1 className="font-bold text-2xl text-white
+                    mt-10" >Digital IT services</h1>
+                    <p className="text-gray-200 p-4 text-sm ">Our team of experts is dedicated to providing top-tier It services
+                        to help your business grow and succees in the digital world. We offer a wide range
+                        of services to meet your business needs. from website design and development to software
+                        development and digital marketing.
+                    </p>
+                    <button className="flex justify-center items-center rounded-3xl mt-8 h-fit w-fit py-2 px-8 text-lg text-white bg-darkBlue ">Contact Us </button>
+                </div>
+                <div className="p-4 mt-10">
+                    <img src="webdesign.webp" />
+                    <div className="mt-10">
+                        <h2 className="font-bold text-2xl text-primaryGreen">
+                            Website Design and Development</h2>
+                        <p className="p-4 font-light text-sm">
+                        Despite having a fully functional website, often business firms fail to make a
+                        significant mark among their targeted market. Investing in the right areas will certainly lead your
+                        business towards a promising success and help you climb higher on the ladder of market competition.
+                        With FinInfocom, hitting the right note and making an impressive web presentation is possible
+                        through our exclusive services. Being a web application service provider in this domain for many
+                        years, we have the right blend of talent and industry insight to make your business flourish and
+                        emerge stronger. We feel pride to we have a wide range of business firms from various types of
+                        industries as our clients.
+
+
+                        </p>
+
+                    </div>
+                </div>
+                <div className="mt-6 p-4">
+                    <div className="mt-10">
+                        <h2 className="font-bold text-2xl text-primaryGreen">
+                            Our expertise in Web Application Development</h2>
+                        <p className="p-4 font-light text-sm">
+                            We understand that a website is the face of your business in the digital world.
+                            That's why we provide professional website design and development services to
+                            help you create a stunning and user-friendly website that engages your customers
+                            and drives sales. Our team of designers and developers work closely with you to
+                            create a website that meets your business requirements and reflects your brand
+                            image.
+
+
+                        </p>
+
+                    </div>
+                    <img src="applications.png" />
+
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default IteServices
