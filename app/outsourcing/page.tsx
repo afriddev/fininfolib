e4320581bd479d521a0254e13b877cb3a5c83477
AFRID SHAIK <afridayan01@gmail.com>
2023-10-09 12:31:33 +0000

    Add files via upload

---
+++ app/outsourcing/page.tsx
@@ -0,0 +1,35 @@
+const OutsourcingServices: () => void = () => {
+
+
+    return (
+        <>
+            <div>
+                <div
+                    className="flex flex-col space-y-4 text-white p-4 bg-gradient-to-b from-o1 to-o2 w-full h-fit">
+                    <label className="text-white font-bold text-3xl mt-6 ">OutSourcing Services</label>
+                    <p className="text-gray-100 text-sm ">As a leading IT outsourcing company, we are committed to helping businesses achieve their
+                        goals by providing them with access to skilled and cost-effective resources that can improve
+                        their efficiency. Whether you need to outsource a single task or an entire project, we can help
+                        you find the right solution to meet your business needs.</p>
+                    <p className="text-gray-100 text-sm ">We provide a dedicated team of tech employees to work on your projects
+                        , ensuring that you have access to the right talent to complete your projects on
+                        time and within budget. Our team of experts specializes in a wide range of technologies and can
+                        help you develop custom software solutions, build websites
+                        , manage your IT infrastructure, and more.</p>
+                        <img className="p-6" src="outservices.webp" />
+                </div>
+                <div className="p-4 flex flex-col ">
+                    <img src="techemployees.webp" />
+                    <div>
+                        <h2>Dedicated Tech Employees</h2>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+
+
+export default OutsourcingServices
