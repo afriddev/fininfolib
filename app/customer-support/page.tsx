11402761c6ccb0ed8c70d99f346cfa0a2aa340cb
AFRID SHAIK <afridayan01@gmail.com>
2023-10-10 01:29:18 +0000

    Add files via upload

---
+++ app/customer-support/page.tsx
@@ -0,0 +1,119 @@
+const CustomerSupport:()=>void = ()=>{
+
+    return(
+        <>
+            <div>
+                <div className="flex flex-col w-full bg-gradient-to-b from-o1 to-o2 
+                p-6 ">
+                    <h1 className="text-white font-bold text-xl text-center">Tele services Fully Managed for 
+                        Sales / Customer / Technical support and Operations Teams.</h1>
+                        <p className="text-sm text-gray-200 p-4">
+                        Fininfolib has established partnerships with clients to 
+                        facilitate the smooth transition of critical business processes
+                         to our state-of-the-art BPO service delivery center. Our track
+                          record speaks for itself, as we have consistently provided 
+                          high-value outsourcing services to various industries.
+                        </p>
+                </div>
+                <div className="p-6 mt-4">
+                    <img src="partnership.webp" />
+                    <p className="pt-4 tetx-md text-gray-700" >
+                    Fininfolib has established partnerships with clients to facilitate 
+                    the smooth transition of critical business processes to our state-of-the-art BPO
+                     service delivery center. Our track record speaks for itself, as we have 
+                     consistently
+                     provided high-value outsourcing services to various industries<br/>
+
+                     Fininfolib has established strategic alliances with clients to ensure a
+                      smooth transition of critical business processes to our cutting-edge BPO service delivery 
+                      center.Our track record speaks for itself, as we have provided high-value outsourcing services
+                       to a variety of industries on a consistent basis. 
+
+                    
+                    </p>
+                    <button className="mt-8 ml-4 mb-10 rounded-xl flex justify-center items-center text-center bg-o1 text-white w-fit h-fit 
+                    py-[8px] px-6">Contact Us</button>
+                    <p className="pt-4 tetx-md text-gray-800">
+                    At Fininfolib, we offer an extensive range of business process services
+                     to industry leaders in multiple countries, including India, UK, USA, Australia, Middle East,
+                      and Southeast Asia. Our primary objective is to provide end-user customer satisfaction while 
+                      ensuring optimal ROI for our
+                     clients in both front and back-office outsourcing requirements.
+                    </p>
+                    <p className="pt-4 tetx-md text-gray-800">
+                    At Fininfolib, we provide customized BPO services that cater to every business requirement. 
+                    Our track record speaks for itself, as we have consistently delivered exceptional BPO services to 
+                    various industries. Our main focus is to provide a delightful customer experience while ensuring optimal
+                     ROI for our clients. With over 15 years of experience, our support team members offer high-quality 
+                     outsourcing solutions,
+                     making us the preferred BPO service provider for businesses.
+                    </p>
+                    <img className="mt-4 p-4" src="business.webp"/>
+                </div>
+                <div className="flex flex-col p-4 mt-6 ">
+                    <div className="flex flex-col p-4 space-y-2 border border-primaryGreen rounded-2xl">
+                        <img className="w-20 h-24 mt-4"  src="chatsupport.png" />
+                        <h2 className="font-bold text-primaryGreen text-xl ">Live Chat Support Services</h2>
+                        <p className="text-sm tex-gray-600 font-light pb-4">
+                        Fininfocom's live chat support services offer a strategic and
+                         cost-effective solution to provide 24/7 live chat support to our clients.
+                          Our expert professionals take the time to understand your requirements and
+                           provide convincing answers to resolve customer queries, ultimately persuading 
+                           them to convert. With our live chat support services, you can obtain real-time 
+                           assistance, convert visitors into customers, reduce cart abandonment rates, and
+                            gather detailed information about your visitors.
+                        </p>
+                    </div>
+                    <div className="flex flex-col p-4 space-y-2 border border-primaryGreen rounded-2xl">
+                        <img className="w-20 h-24 mt-4"  src="customer.png" />
+                        <h2 className="font-bold text-primaryGreen text-xl ">Customer Support Services</h2>
+                        <p className="text-sm tex-gray-600 font-light pb-4">
+                        At Fininfocom, our certified and experienced team provides
+                         round-the-clock customer support that effectively boosts your sales,
+                          allowing you to outsource customer services and focus on growing your business.
+                           Our customer support team provides assistance across multiple channels, enhances
+                            productivity, saves time and money, and ultimately boosts your customer 
+                            satisfaction. With our services, you can rest assured that your customers are 
+                            receiving top-notch support, while you concentrate on scaling your business.
+                        
+                        </p>
+                    </div>
+                    <div className="flex flex-col p-4 space-y-2 border border-primaryGreen rounded-2xl">
+                        <img className="w-20 h-24 mt-4"  src="call-center.png" />
+                        <h2 className="font-bold text-primaryGreen text-xl ">Product Support Services</h2>
+                        <p className="text-sm tex-gray-600 font-light pb-4">
+                        At Fininfocom, our customer service outsourcing company provides a
+                         broad range of product support services focused on delivering excellent
+                          customer satisfaction. Our expert team fulfills the specific needs of businesses
+                           with our customized product support services and system-level reports.
+                            We understand the importance of providing timely and effective product support
+                             to customers, and we work diligently to ensure that their needs are met. With
+                              our services, you can rest assured that your customers are receiving top-notch
+                               product support, allowing you to focus on other aspects of your business.
+                        
+                        </p>
+                    </div>
+                    <div className="flex flex-col p-4 space-y-2 border border-primaryGreen rounded-2xl">
+                        <img className="w-20 h-24 mt-4"  src="chatsupport.png" />
+                        <h2 className="font-bold text-primaryGreen text-xl ">Live Chat Support Services</h2>
+                        <p className="text-sm tex-gray-600 font-light pb-4">
+                        Fininfocom's live chat support services offer a strategic and
+                         cost-effective solution to provide 24/7 live chat support to our clients.
+                          Our expert professionals take the time to understand your requirements and
+                           provide convincing answers to resolve customer queries, ultimately persuading 
+                           them to convert. With our live chat support services, you can obtain real-time 
+                           assistance, convert visitors into customers, reduce cart abandonment rates, and
+                            gather detailed information about your visitors.
+                        </p>
+                    </div>
+                    
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+
+export default CustomerSupport
