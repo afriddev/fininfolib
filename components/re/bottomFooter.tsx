17a9342490019bbed85c859a6a1f934a68e805d8
AFRID SHAIK <afridayan01@gmail.com>
2023-10-15 02:57:51 +0000

    Add files via upload

--- components/re/bottomFooter.tsx
+++ components/re/bottomFooter.tsx
@@ -3,32 +3,16 @@ import {CiFacebook,CiInstagram,CiLinkedin} from 'react-icons/ci'
 export default function BottomFooter() {
     return (
         <>
-            <div className="w-full flex flex-col bg-darkBlue text-gray-300 justify-center 
+            <div className="w-full flex flex-col bg-sH text-white justify-center 
             items-center p-4 text-xs">
-                <h2 className='text-2xl text-primaryGreen'>FIN INFOLIB</h2>
-                <div className='w-full mt-6'>
-                    <ul className='w-full flex justify-between border-b border-gray-400 pb-2 mb-2
-                    '>
-                        <li><a>About Us</a></li>
-                        <li><a>Services</a></li>
-                        <li><a>Terms & Conditions</a></li>
-                        
-                    </ul>
-                </div>
-                <div className='w-full '>
-                    <ul className='w-full flex justify-between pb-4'>
-                        <li><a>Privacy Policy </a></li>
-                    <li><a>Disclaimer</a></li>
-                    <li><a>Accessibility</a></li>
-                    
-                    </ul>
-                </div>
+                <h2 className='text-3xl text-pH font-bold '>FIN INFOLIB</h2>
+                
                 <div className="w-2/5">
                     <ul className='flex justify-between'>
                         
-                        <a className='text-primaryGreen w-fit h-fi px-2 py-2 text-3xl font-bold'><CiFacebook /></a>
-                        <a className='text-primaryGreen w-fit h-fi px-2 py-2 text-3xl font-bold'><CiInstagram/></a>
-                        <a className='text-primaryGreen w-fit h-fi px-2 py-2 text-3xl font-bold'><CiLinkedin/></a>
+                        <a className='text-white bg-gray-800 rounded-md w-fit h-fit px-1 py-1 text-3xl font-bold'><CiFacebook /></a>
+                        <a className='text-white bg-gray-800 rounded-md w-fit h-fit px-1 py-1 text-3xl font-bold'><CiInstagram/></a>
+                        <a className='text-white bg-gray-800 rounded-md w-fit h-fit px-1 py-1 text-3xl font-bold'><CiLinkedin/></a>
                     </ul>
 
                 </div>
