3d40b3e5ddc9376d68be027f760638ad9761f9c7
AFRID SHAIK <afridayan01@gmail.com>
2023-10-16 10:23:35 +0000

    Add files via upload

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,37 @@
+"use client";
 import DashboardMain from "@/components/dashboard/dashboardMain"
 import EmployeeMain from "@/components/dashboard/employeeMain."
 import InboxMain from "@/components/dashboard/inboxMain"
 import JobsMain from "@/components/dashboard/jobsMain"
 import Sidepanel from "@/components/dashboard/sidepanel"
+import { useState } from "react"
 
 const Dashboard = () => {
-    return (
+    const [index,setIndex] = useState(0)
+    function setindex(val:number){
+        setIndex(val)
+
+    }
+    return ( 
         <>
             <main className=" w-full h-[90vh] bg-white">
                 <section>
                     <div className="flex">
-                        <Sidepanel />
-                        <JobsMain/>
-                        {/* <InboxMain/> */}
-                        {/* <EmployeeMain/> */}
-                        {/* <DashboardMain /> */}
+                        <Sidepanel index={index} setindex={setindex} />
+                        {
+                            index == 0
+                            ? <DashboardMain /> 
+                            :index == 1
+                            ?<InboxMain/>
+                            :index==2
+                            ?<EmployeeMain/>
+                            :index == 3
+                            ?<JobsMain/>
+                            :null
+                        }
+                        
+                        
+                        
                     </div>
                 </section>
             </main>
--- components/dashboard/sidepanel.tsx
+++ components/dashboard/sidepanel.tsx
@@ -4,37 +4,42 @@ import { BiUser } from "react-icons/bi"
 import { HiOutlineInboxArrowDown } from "react-icons/hi2"
 import { IoSettingsOutline } from "react-icons/io5"
 
-const Sidepanel = ()=>{
-    return(
+const Sidepanel = ({ setindex, index }) => {
+    return (
         <>
             <section className=" w-fit pr-16 overflow-hidden">
                 <div className=" w-fit  h-[90vh] bg-white px-10 py-4 border-r border-zinc-400 shadow-lg">
-                    <div className="flex flex-col gap-y-20 pt-10 ">
-                        <div className="flex  justify-center w-fit items-center  text-purple-600 bg-purple-100  h-fit
-                        px-10 py-2 gap-x-4 rounded-full">
+                    <div className="flex flex-col gap-y-32 pt-10 ">
+                        <div onClick={
+                            () => { setindex(0) }
+                        } className={index == 0 ? "flex  justify-center w-fit items-center  text-purple-600 bg-purple-100  h-fit px-10 py-2 gap-x-4 rounded-full" :
+                            "flex  justify-center w-fit items-center  text-zinc-600  h-fit px-10 py-2 gap-x-4 rounded-full"}>
                             <a className=" text-3xl "><BiSolidDashboard /></a>
+
                             <label className="text-2xl pt-1  font-mono text-center   ">Dashboard</label>
                         </div>
-                        <div className="flex  justify-center w-fit items-center  text-zinc-600  h-fit
-                        px-10 py-2 gap-x-4 rounded-full">
+                        <div onClick={
+                            () => { setindex(1) }
+                        } className={index == 1 ? "flex  justify-center w-fit items-center  text-purple-600 bg-purple-100  h-fit px-10 py-2 gap-x-4 rounded-full" :
+                            "flex  justify-center w-fit items-center  text-zinc-600  h-fit px-10 py-2 gap-x-4 rounded-full"}>
                             <a className=" text-3xl "><BsInbox /></a>
                             <label className="text-2xl pt-1  font-mono text-center   ">Inbox</label>
                         </div>
-                        <div className="flex  justify-center w-fit items-center  text-zinc-600  h-fit
-                        px-10 py-2 gap-x-4 rounded-full">
+                        <div onClick={
+                            () => { setindex(2) }
+                        } className={index == 2 ? "flex  justify-center w-fit items-center  text-purple-600 bg-purple-100  h-fit px-10 py-2 gap-x-4 rounded-full" :
+                            "flex  justify-center w-fit items-center  text-zinc-600  h-fit px-10 py-2 gap-x-4 rounded-full"}>
                             <a className=" text-3xl "><BiUser /></a>
                             <label className="text-2xl pt-1  font-mono text-center   ">Employees</label>
                         </div>
-                        <div className="flex  justify-center w-fit items-center  text-zinc-600  h-fit
-                        px-10 py-2 gap-x-4 rounded-full">
+                        <div onClick={
+                            () => { setindex(3) }
+                        } className={index == 3 ? "flex  justify-center w-fit items-center  text-purple-600 bg-purple-100  h-fit px-10 py-2 gap-x-4 rounded-full" :
+                            "flex  justify-center w-fit items-center  text-zinc-600  h-fit px-10 py-2 gap-x-4 rounded-full"}>
                             <a className=" text-3xl "><HiOutlineInboxArrowDown /></a>
                             <label className="text-2xl pt-1  font-mono text-center   ">Jobs</label>
                         </div>
-                        <div className="flex  justify-center w-fit items-center  text-zinc-600  h-fit
-                        px-10 py-2 gap-x-4 rounded-full">
-                            <a className=" text-3xl "><IoSettingsOutline /></a>
-                            <label className="text-2xl pt-1  font-mono text-center   ">Settings</label>
-                        </div>
+
                     </div>
                 </div>
             </section>
