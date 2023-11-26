0a1d63cdb054f11d71f5723f9a73999b7035e2f9
AFRID SHAIK <afridayan01@gmail.com>
2023-11-26 06:37:05 +0000

    improved mobile navgation responsive

--- components/re/navBar.tsx
+++ components/re/navBar.tsx
@@ -11,8 +11,8 @@ export default function NavBar() {
   return (
     <>
       <div className=" mb-[-2px]">
-        <nav className="flex justify-between m-4 items-center ">
-          <label className="text-primaryGreen font-bold text-2xl ">
+        <nav className="flex justify-between m-4 items-center pl-4 ">
+          <label className="text-primaryGreen font-bold text- pl-4 ">
             FIN INFOLIB
           </label>
           <a className=" lg:hidden">
@@ -85,44 +85,25 @@ function LapView({ setopen }) {
 
 
 function MobileNav({ setopen }) {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
 
   return (
     <>
       <div
-        ref={ref}
-        style={{
-          transform: isInView ? "none" : "translateX(-100vw)",
-          transition: "all 0.9s cubic-bezier(0.6,0.01,0.05,0.95) 1s"
-        }}
-        className="absolute flex flex-col  z-50 w-[100vw] h-[35vh] bg-white top-[11vh] right-0  p-4">
-        <div className="flex flex-col h-[35vh] justify-between pl-10 pr-4">
-          <li className=" list-none flex flex-col gap-y-4 text-lg  justify-center items-center ">
-            <Link onClick={setopen} className=" border-b border-gray-700"
-              style={{
-                transform: isInView ? "none" : "translateX(-100vw)",
-                transition: "all 0.7s cubic-bezier(0.6,0.01,0.05,0.95) 1.3s"
-              }} href={'/'}>Home</Link>
-            <Link onClick={setopen} className=" border-b border-gray-700"
-              style={{
-                transform: isInView ? "none" : "translateX(-100vw)",
-                transition: "all 0.7s cubic-bezier(0.6,0.01,0.05,0.95) 1.6s"
-              }} href={'/about'}>About</Link>
-            <Link onClick={setopen} className=" border-b border-gray-700"
-              style={{
-                transform: isInView ? "none" : "translateX(-100vw)",
-                transition: "all 0.7s cubic-bezier(0.6,0.01,0.05,0.95) 1.9s"
-              }} href={'/contact'}>Contact</Link>
-            <Link onClick={setopen} className=" border-b border-gray-700"
-              style={{
-                transform: isInView ? "none" : "translateX(-100vw)",
-                transition: "all 0.9s cubic-bezier(0.6,0.01,0.05,0.95) 2.2s"
-              }} href={'/itservices'}>It Services</Link>
+        className="absolute flex flex-col  z-50 w-fit h-fit py-2 bg-white top-[10vh] right-0  p-4">
+        <div className="flex flex-col h-fit justify-between ">
+          <li className=" list-none flex flex-col space-y-4 pt-4  justify-end items-center text-center    " >
+            <Link onClick={setopen} className=" "
+             href={'/'}>Home</Link>
+            <Link onClick={setopen} className=" "
+             href={'/about'}>About</Link>
+            <Link onClick={setopen} className=" "
+              href={'/contact'}>Contact</Link>
+            <Link onClick={setopen} className=" "
+             href={'/itservices'}>It Services</Link>
 
           </li>
-          <label className="text-[15px] font-light text-gray-600 text-center ">Employee ?
-            <span className="text-[17px] text-pH font-normal"> <Link onClick={setopen} href={'/login'}>Login</Link></span></label>
+          <label className="text-xs font-light text-gray-600 text-center pt-6 pb-2 ">Employee ?
+            <span className="text-sm text-pH font-normal"> <Link onClick={setopen} href={'/login'}>Login</Link></span></label>
         </div>
 
 
